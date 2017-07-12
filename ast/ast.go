4f1276d86583ef7036b2ba427ddd8786e52c0f34
Antoine Martin <antoine97.martin@gmail.com>
2017-07-12 22:08:09 +0200

    Add AST types file

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "github.com/alarsyo/monkey/token"
+
+// Node of the Abstract Syntax Tree
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement node of the AST
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression node of the AST
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of the AST
+type Program struct {
+	Statements []Statement
+}
+
+// LetStatement node of the AST
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral returns the token of the let statement (LET)
+func (ls *LetStatement) TokenLiteral() (s string) { return ls.Token.Literal }
+
+// Identifier node of the AST
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+//TokenLiteral returns the token of the identifier (IDENT)
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
