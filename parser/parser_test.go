25d0d204bd407d74df937ae1e5cfe5b136abed3a
Antoine Martin <antoine97.martin@gmail.com>
2017-07-12 23:11:45 +0200

    Parser now passes basic tests
    Still not functional

---
+++ parser/parser_test.go
@@ -0,0 +1,68 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/alarsyo/monkey/ast"
+	"github.com/alarsyo/monkey/lexer"
+)
+
+func TestLetStatement(t *testing.T) {
+	const statementNumber = 3
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram returned nil")
+	}
+	if len(program.Statements) != statementNumber {
+		t.Fatalf("program does not contain %d statements. got %d",
+			statementNumber, len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+
+	return true
+}
